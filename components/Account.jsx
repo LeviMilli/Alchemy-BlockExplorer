f382f7a9c7dab8edb885bd1bea8bd8a63b18b421
Levi Milli <levi.millikin@gmail.com>
2023-06-26 08:52:41 -0600

    block explorer

---
+++ components/Account.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function Account() {
+const [account, setAccount] = useState()
+
+
+
+  return (
+    <div>Account</div>
+  )
+}
+
+export default Account
--- src/App.js
+++ src/App.js
@@ -1,36 +1,114 @@
 import { Alchemy, Network } from 'alchemy-sdk';
 import { useEffect, useState } from 'react';
-
+import Web3 from 'web3';
 import './App.css';
 
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
+const web3 = new Web3();
+
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
 
-
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [trans, setTrans] = useState();
+  const [showTransactions, setShowTransactions] = useState(false);
+  const [accountLook, setAccountLook] = useState()
+  const [display, setDisplay] = useState()
+  const [nft, setNft] = useState()
+  const [nftSearch, setNftSearch] = useState()
+
 
   useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+    async function fetchData() {
+      try {
+        const blockNumber = await alchemy.core.getBlockNumber();
+        setBlockNumber(blockNumber);
+  
+        const transactions = await alchemy.core.getBlockWithTransactions(blockNumber);
+        setTrans(transactions);
+      } catch (error) {
+        // Handle error
+      }
     }
+  
+    fetchData();
+  }, []);
 
-    getBlockNumber();
-  });
+  const handleBlockClick = () => {
+    setShowTransactions(!showTransactions);
+  };
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  async function accountSearch(){
+    const res = await alchemy.core.getBalance(accountLook, "latest")
+    const ether = web3.utils.fromWei(res.toString(), 'ether');
+    setDisplay(ether)
+  }
+
+  async function floorPrice() {
+    const floor = await alchemy.nft.getFloorPrice(nftSearch)
+    console.log(floor)
+    setNft(floor)
+
+  }
+
+
+
+
+
+  return (
+    <div className="App">
+      <div>
+        <div id = "center"
+          onClick={handleBlockClick}
+          style={{ cursor: 'pointer', textDecoration: 'underline' }}
+        >
+          <h1>Block Number:</h1> <h2>{blockNumber ? blockNumber : "LOADING"}
+          ↓</h2></div>
+        {showTransactions && trans && trans.transactions ? (
+          <div>
+
+           <h1 id='space'> Block Transactions: </h1>
+            {trans.transactions.slice(0, 10).map((tx) => {
+              const value = tx.value.toString();
+              const ethValue = web3.utils.fromWei(value, 'ether');
+
+              return (
+                <div id="space" key={tx.hash}>
+                  <div>
+                    from: {tx.from}
+                    <br></br>
+                    to: {tx.to}
+                    <br></br>
+                    value: {ethValue} ETH
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          "Click on the block number to show the last 10 transactions"
+        )}
+      </div>
+      <div>
+        <h1>Account Balance Lookup:</h1>
+        <input value={accountLook} onChange={(e) => setAccountLook(e.target.value)} />
+        <div>balance:{display}</div>
+        <button onClick={accountSearch}>Search</button>
+      </div>
+      <div>
+        <h1>floor price of nft search:</h1>
+        <button onClick={floorPrice}>Search</button>
+        <input value={nftSearch} onChange={(e) => setNftSearch(e.target.value)} />
+        <h2>nft floor price: {nft ? nft.openSea.floorPrice : ""} ETH</h2>
+
+
+      </div>
+    </div>
+  );
 }
 
 export default App;
